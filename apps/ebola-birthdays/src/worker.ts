db9657720cccea58bdcd0ab1a2c00a5e2d0b9c65
Juan Velasquez <juseve200@gmail.com>
2023-08-07 14:39:41 -0500

    chore: add sentry

--- apps/ebola-birthdays/src/worker.ts
+++ apps/ebola-birthdays/src/worker.ts
@@ -1,9 +1,12 @@
+import { Toucan } from 'toucan-js';
 export interface Env {
 	SHITTY_API_URL: string;
 	SHITTY_API_USERNAME: string;
 	SHITTY_API_PASSWORD: string;
 	BOTNORREA_WEBHOOK: string;
 	BOTNORREA_AUTH: string;
+	SENTRY_DSN: string;
+	STAGE: string;
 }
 
 interface BirthdayResult {
@@ -11,31 +14,36 @@ interface BirthdayResult {
 }
 
 export default {
-	async scheduled(_event: ScheduledEvent, env: Env, _ctx: ExecutionContext): Promise<void> {
-		console.log('Looking for birthdays');
-		const birthdayResponse = await fetch(env.SHITTY_API_URL + '/users/birthdays', {
-			headers: { Authorization: 'Basic ' + btoa(env.SHITTY_API_USERNAME + ':' + env.SHITTY_API_PASSWORD) },
-		});
+	async scheduled(_event: ScheduledEvent, env: Env, context: ExecutionContext): Promise<void> {
+		const sentry = new Toucan({ dsn: env.SENTRY_DSN, context, environment: env.STAGE, enabled: env.STAGE === 'production' });
+		try {
+			console.log('Looking for birthdays');
+			const birthdayResponse = await fetch(env.SHITTY_API_URL + '/users/birthdays', {
+				headers: { Authorization: 'Basic ' + btoa(env.SHITTY_API_USERNAME + ':' + env.SHITTY_API_PASSWORD) },
+			});
 
-		if (!birthdayResponse.ok) {
-			console.log('Failed to get the birtday users');
-			return;
+			if (!birthdayResponse.ok) {
+				console.log('Failed to get the birtday users');
+				return;
+			}
+
+			const birthday = (await birthdayResponse.json()) as BirthdayResult;
+			if (!birthday.users?.length) {
+				console.log('No birthdays today :(');
+				return;
+			}
+
+			console.log('Happy birthday', birthday.users);
+			const birtdayMessage = 'Feliz pumpesito 🥳🎉 ' + birthday.users.map((user) => '@' + user).join(' ');
+
+			await fetch(env.BOTNORREA_WEBHOOK, {
+				method: 'POST',
+				headers: { Authorization: env.BOTNORREA_AUTH },
+				body: JSON.stringify({ message: birtdayMessage }),
+			});
+		} catch (error) {
+			sentry.captureException(error);
 		}
-
-		const birthday = (await birthdayResponse.json()) as BirthdayResult;
-		if (!birthday.users?.length) {
-			console.log('No birthdays today :(');
-			return;
-		}
-
-		console.log('Happy birthday', birthday.users);
-		const birtdayMessage = 'Feliz pumpesito 🥳🎉 ' + birthday.users.map((user) => '@' + user).join(' ');
-
-		await fetch(env.BOTNORREA_WEBHOOK, {
-			method: 'POST',
-			headers: { Authorization: env.BOTNORREA_AUTH },
-			body: JSON.stringify({ message: birtdayMessage }),
-		});
 	},
 
 	async fetch(_event: FetchEvent) {
